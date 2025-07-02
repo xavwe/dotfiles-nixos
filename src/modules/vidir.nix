81499e469d060028d18d4fab0cb60eb4ac9b5885
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-07-02 22:16:50 +0200

    feat: add vidir

---
+++ src/modules/vidir.nix
@@ -0,0 +1,18 @@
+# find . -type f | vidir -
+# vidir
+
+{
+  config,
+  pkgs,
+  inputs,
+  outputs,
+  lib,
+  home-manager,
+  overlays,
+  sops-nix,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    perl540Packages.vidir
+  ];
+}
