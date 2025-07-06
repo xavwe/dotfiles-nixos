73cc2009d98c8da864e7b42ef4fc6bb2a1f4899c
xavwe <xaver.wenhart@proton.me>
2025-07-06 21:29:02 +0200

    feat: add ncmpcpp visualizer

--- src/modules/mpd.nix
+++ src/modules/mpd.nix
@@ -18,8 +18,14 @@
       port "6600"
       restore_paused "yes"
       audio_output {
-              type            "pipewire"
-              name            "PipeWire Sound Server"
+        type    "pipewire"
+        name    "PipeWire Sound Server"
+      }
+      audio_output {
+        type    "fifo"
+        name    "my_fifo"
+        path    "/tmp/mpd.fifo"
+        format  "44100:16:2"
       }
     '';
     musicDirectory = "/home/nu/music";
