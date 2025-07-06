810d5aa4e6e19a7128711f6617b9ff722af9b847
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-07-06 21:29:02 +0200

    feat: add ncmpcpp visualizer

--- src/modules/ncmpcpp.nix
+++ src/modules/ncmpcpp.nix
@@ -18,6 +18,11 @@
       settings = {
         mpd_host = "localhost";
         mpd_port = "6600";
+        visualizer_data_source = "/tmp/mpd.fifo";
+        visualizer_output_name = "my_fifo";
+        visualizer_in_stereo = "yes";
+        visualizer_type = "ellipse";
+        visualizer_look = "●▮";
       };
     };
   };
