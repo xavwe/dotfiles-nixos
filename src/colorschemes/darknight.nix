d5adabb2d9532bcd1c3b6abcbcb3e37ba97a314f
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-07-07 18:39:34 +0200

    feat: move colorschemes to extra folder

---
+++ src/colorschemes/darknight.nix
@@ -0,0 +1,22 @@
+{
+  # Darknight Colorscheme
+  # This is a basic dark theme, feel free to customize it.
+  darknight = {
+    base00 = "1d1f21"; # black
+    base01 = "282a2e"; # dark grey
+    base02 = "373b41"; # medium grey
+    base03 = "969896"; # light grey
+    base04 = "b4b7b4"; # off white
+    base05 = "c5c8c6"; # white
+    base06 = "e0e0e0"; # light white
+    base07 = "ffffff"; # pure white
+    base08 = "cc6666"; # red
+    base09 = "de935f"; # orange
+    base0A = "f0c674"; # yellow
+    base0B = "b5bd68"; # green
+    base0C = "8abeb7"; # cyan
+    base0D = "81a2be"; # blue
+    base0E = "b294bb"; # magenta
+    base0F = "a3685a"; # brown
+  };
+}
