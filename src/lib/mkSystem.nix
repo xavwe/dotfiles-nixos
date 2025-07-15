e7f2d1e6a53e1a24b120e45ec3ee4ee42729d932
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-07-15 03:32:37 +0200

    chore: restructure flake.nix using new mkSystem util

---
+++ src/lib/mkSystem.nix
@@ -0,0 +1,32 @@
+# This function builds a NixOS system configuration or a generator package.
+# It is parameterized by profile and hardware, and optionally by system and format.
+{ nixpkgs, self, inputs }:
+({ profile, hardware, system ? "x86_64-linux", format ? null }:
+let
+  systemConfig = {
+    specialArgs = {
+      inherit inputs;
+      lib = nixpkgs.lib // self.lib;
+      overlays = import ../overlays {inherit inputs;};
+    };
+    modules = [
+      ../modules
+      ../profiles/${profile}.nix
+      ../hardware/${hardware}.nix
+      ../colorschemes
+      inputs.disko.nixosModules.disko
+      inputs.sops-nix.nixosModules.sops
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager.users.nu = {
+          imports = [inputs.nvf.homeManagerModules.default];
+        };
+      }
+    ];
+  };
+in
+# If format is null, build a nixosSystem, otherwise a generator package.
+if format == null then
+  nixpkgs.lib.nixosSystem systemConfig
+else
+  inputs.nixos-generators.nixosGenerate (systemConfig // { inherit system format; }))
