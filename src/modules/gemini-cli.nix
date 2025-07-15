5001ab62007d045df644c94e0527574394972e9a
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-07-15 19:11:54 +0200

    feat: make gemini use api

--- src/modules/gemini-cli.nix
+++ src/modules/gemini-cli.nix
@@ -9,14 +9,21 @@
   sops-nix,
   ...
 }: {
+  sops.secrets.gemini = {
+    owner = "nu";
+    mode = "0400";
+  };
   environment.systemPackages = with pkgs; [
-    gemini-cli
+    (pkgs.writeShellScriptBin "gemini" ''
+      export GEMINI_API_KEY="$(cat ${config.sops.secrets.gemini.path})"
+      exec ${pkgs.gemini-cli}/bin/gemini "$@"
+    '')
   ];
   home-manager.users.nu = {...}: {
     home.file.".gemini/settings.json".text = ''
       {
         "theme": "GitHub",
-        "selectedAuthType": "oauth-personal",
+        "selectedAuthType": "gemini-api-key"",
         "usageStatisticsEnabled": false
       }
     '';
