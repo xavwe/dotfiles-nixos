6a6a9c8482dce9a46de9b0d52162e459dae65de2
xavwe <xaver.wenhart@proton.me>
2025-07-16 00:31:15 +0200

    feat: add kitty terminal emulator module

---
+++ src/modules/kitty.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  home-manager,
+  ...
+}: {
+  options.modules.kitty = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use kitty";
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf config.modules.kitty.enable {
+      home-manager.users.nu = {...}: {
+        programs.kitty = {
+          enable = true;
+          font = {
+            name = "JetBrainsMono Nerdfont";
+            size = 13;
+          };
+          settings = {
+            scrollback_lines = 5000;
+          };
+        };
+      };
+    })
+  ];
+}
