de451f5a40392a319ef7019c40cab095c19f1cc9
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-07-16 22:41:23 +0200

    feat(gh): add GitHub CLI module
    Add new Home Manager module for GitHub CLI (gh) with SSH git protocol and less pager configuration. Enable module in desktop profile.

---
+++ src/modules/gh.nix
@@ -0,0 +1,29 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.modules.gh = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use GitHub CLI (gh)";
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf config.modules.gh.enable {
+      home-manager.users.nu = {...}: {
+        programs.gh = {
+          enable = true;
+          settings = {
+            git_protocol = "ssh";
+            pager = "less";
+          };
+        };
+      };
+    })
+  ];
+}
