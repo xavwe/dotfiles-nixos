e93a586b4600d148226f75f9d9f3db13b3c8f426
xavwe <xaver.wenhart@proton.me>
2025-07-18 21:57:39 +0200

    feat(zsh): add OSC-7 escape sequence for directory tracking
    - Add osc7-pwd function to emit OSC-7 escape sequences
    - Add chpwd-osc7-pwd hook to track directory changes
    - Enable terminal applications to track current working directory

--- src/modules/zsh.nix
+++ src/modules/zsh.nix
@@ -53,6 +53,18 @@
               screenshot() {
                 IMG=~/screenshot/$(date +%Y-%m-%d_%H-%m-%s).png && grim -g "$(slurp)" $IMG && wl-copy < $IMG
               }
+
+              # OSC-7 escape sequence for terminal working directory tracking
+              function osc7-pwd() {
+                emulate -L zsh # also sets localoptions for us
+                setopt extendedglob
+                local LC_ALL=C
+                printf '\e]7;file://%s%s\e\' $HOST ''${PWD//(#m)([^@-Za-z&-;_~])/%''${(l:2::0:)$(([##16]#MATCH))}}
+              }
+              function chpwd-osc7-pwd() {
+                (( ZSH_SUBSHELL )) || osc7-pwd
+              }
+              add-zsh-hook -Uz chpwd chpwd-osc7-pwd
             ''
           ];
           profileExtra = lib.mkIf config.modules.hyprland.enable ''
