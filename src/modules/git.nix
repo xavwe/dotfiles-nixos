c353a150115d64bc112ce69c95fbbd8c85814a86
xavwe <xaver.wenhart@proton.me>
2025-08-18 19:48:27 +0200

    fix(git): use full name

--- src/modules/git.nix
+++ src/modules/git.nix
@@ -24,7 +24,7 @@
       home-manager.users.nu = {...}: {
         programs.git = {
           enable = true;
-          userName = "xavwe";
+          userName = "Xaver Wenhart";
           userEmail = "xaver.wenhart@proton.me";
           aliases = lib.mkIf config.modules.git.aliases.yolo {
             yolo = "!git commit -m \"$(curl -s https://whatthecommit.com/index.txt)\"";
