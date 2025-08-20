16237f597288b279df08290e6962e8cef6d2b2af
Xaver Wenhart <git@xavwe.dev>
2025-08-20 13:33:27 +0200

    feat(tldr): add update service

--- src/modules/tldr.nix
+++ src/modules/tldr.nix
@@ -20,6 +20,10 @@
         home.packages = with pkgs; [
           tealdeer
         ];
+        services.tldr-update = {
+          enable = true;
+          package = pkgs.tealdeer;
+        };
       };
     })
   ];
