6ff5afdda15b7322e147e33fefd2df36cacde15e
Xaver Wenhart <git@xavwe.dev>
2025-09-13 10:42:58 +0200

    refactor(ssh): add default config as programs.ssh.enableDefaultConfig will be deprecated

--- src/modules/ssh.nix
+++ src/modules/ssh.nix
@@ -67,6 +67,18 @@
                 IdentityFile = "~/.ssh/codeberg";
               };
             };
+            "*" = {
+              forwardAgent = false;
+              addKeysToAgent = "no";
+              compression = false;
+              serverAliveInterval = 0;
+              serverAliveCountMax = 3;
+              hashKnownHosts = false;
+              userKnownHostsFile = "~/.ssh/known_hosts";
+              controlMaster = "no";
+              controlPath = "~/.ssh/master-%r@%n:%p";
+              controlPersist = "no";
+            };
           };
         };
       };
